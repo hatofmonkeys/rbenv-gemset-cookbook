d30c6a93167f2a3ced42c018191fd578bb062ed7
José Airosa <j.airosa@digital-science.com>
2013-06-10 17:31:02 +0100

    Updated metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,13 @@
 name             'rbenv-gemset'
 maintainer       'José P. Airosa'
 maintainer_email 'me@joseairosa.com'
-license          'All rights reserved'
-description      'Installs/Configures rbenv-gemset'
+license          'Apache 2.0'
+description      'Installs and Configures rbenv-gemset'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-%w(rbenv git).each { |d| depends d}
+recipe 'rbenv-gemset', 'Installs and Configures rbenv-gemset'
+
+%w{ centos redhat fedora ubuntu debian amazon oracle}.each { |os| supports os }
+
+%w{ rbenv git }.each { |d| depends d}
